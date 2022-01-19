f5192b195e451c47ca986a17c4275243ca5074c7
Rurikide <79031388+Rurikide@users.noreply.github.com>
2022-01-18 21:18:49 -0500

    ft_strjoin_symbol()

--- libft/libsources/ft_strjoin.c
+++ libft/libsources/ft_strjoin.c
@@ -6,11 +6,33 @@
 /*   By: tshimoda <tshimoda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/02 20:36:30 by tshimoda          #+#    #+#             */
-/*   Updated: 2021/08/09 12:36:50 by tshimoda         ###   ########.fr       */
+/*   Updated: 2022/01/18 21:17:03 by tshimoda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_strjoin_symbol(char *str1, char symbol, char *str2)
+{
+	char	*new_str;
+	int		bytes;
+	int		i;
+
+	i = 0;
+	bytes = ft_strlen(str1) + ft_strlen(str2) + 2;
+	new_str = malloc(sizeof(char) * bytes);
+	while (str1 != NULL && *str1)
+	{
+		new_str[i++] = *(str1++);
+	}
+	new_str[i++] = symbol;
+	while (str2 != NULL && *str2)
+	{
+		new_str[i++] = *(str2++);
+	}
+	new_str[i] = '\0';
+	return (new_str);
+}
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
